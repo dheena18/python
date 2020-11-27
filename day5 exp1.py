808fe17a55aa4a91eda1adf5363fcd6403812b34
dheena18 <dheenadayalant@gmail.com>
2020-11-27 23:08:51 +0530

    Add files via upload

---
+++ day5 exp1.py
@@ -0,0 +1,8 @@
+def arith_ope(a,b):
+    print("addition",a+b)
+    print("subtraction", a-b)
+    print("multiplication", a*b)
+    print("Division", a//b)
+x=int(input("firstnumber="))
+x=int(input("secondnumber="))
+arith_ope(x,y)
---
+++ day5 exp2.py
@@ -0,0 +1,5 @@
+def covid(name,temp=98):
+    print("patient name:",name)
+    print("body temperature:",temp)
+a=input("enter patient name:")
+covid(a)
